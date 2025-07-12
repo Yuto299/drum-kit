12aac406c7e7af98d4781b03f6f1b2de0878324c
Yuto299 <ota.se.ke@outlook.jp>
2025-07-12 12:39:19 +0900

    便利な関数呼び出し

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,7 +1,27 @@
-let numberOfButton = document.querySelectorAll('.drum').length;
+// let numberOfButton = document.querySelectorAll('.drum').length;
 
-for (let i = 0; i < numberOfButton; i++) {
-  document.querySelectorAll('.drum')[i].addEventListener('click', function handleClick() {
-    alert('clicked!');
-  });
+// for (let i = 0; i < numberOfButton; i++) {
+//   document.querySelectorAll('.drum')[i].addEventListener('click', function handleClick() {
+//     alert('clicked!');
+//   });
+// }
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function calculator(num1, num2, operator) {
+  return operator(num1, num2);
 }
