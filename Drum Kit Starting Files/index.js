f903c07322fd265d410a318f1cbc79d774a652c9
Yuto299 <ota.se.ke@outlook.jp>
2025-07-13 12:42:20 +0900

    feat(index.js): キーボードを押したら音が鳴るようにした

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -3,46 +3,55 @@ let numberOfButton = document.querySelectorAll(".drum").length;
 for (let i = 0; i < numberOfButton; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     let buttonInnerHTML = this.innerHTML;
-
-    switch (buttonInnerHTML) {
-      case "w":
-        let tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case "a":
-        let tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case "s":
-        let tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case "d":
-        let tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      case "j":
-        let snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-
-      case "k":
-        let crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case "l":
-        let kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-
-      default:
-    }
+    makeSound(buttonInnerHTML);
   });
+}
+
+// キーボードで押す時の処理（key = キーボードのキー）
+document.addEventListener("keydown", function (e) {
+  console.log(e.buttonInnerHTML);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+  }
 }
 
 // const audio = new Audio("sounds/tom-1.mp3");
